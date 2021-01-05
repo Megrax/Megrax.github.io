1097b4bd981858e74005663bf4d46ea603bebeb3
Megrax <geronimooo@foxmail.com>
2021-01-05 22:50:11 +0800

    update smaller img

--- js/action.js
+++ js/action.js
@@ -7,16 +7,29 @@
  * @FilePath: /Blog-FE/action.js
  */
 
+// $(window).scroll(function(){
+//     let win_h = $(window).scrollTop();
+//     let nav_h = $("header").height();
+//     if(win_h == nav_h){
+//         $("header").css("display",none);
+//     }
+// });
 
 $(window).scroll(function () {
     //获取距离页面顶部的距离
     let toTop =
         document.documentElement.scrollTop ||
         document.body.scrollTop;
+    console.log(toTop);
     //div距离顶部的距离
     if (toTop >= 800) {
         $('#shangxia2').removeClass('hidden');
+        $("header").removeClass('hidden');
+    } else if (toTop === 0) {
+        $("header").addClass('hidden');
+        $('#shangxia2').addClass('hidden');
     } else {
+        $("header").removeClass('hidden');
         $('#shangxia2').addClass('hidden');
     }
 });
